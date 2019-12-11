bee4aca3c5ed3c457fa183ff85e1db21efc1b3d0
JericHunter <jeric.hunterr@gmail.com>
2019-12-11 00:14:26 -0800

    workin on front end

--- gamepro/urls.py
+++ gamepro/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # Admin Site
     path('admin/', admin.site.urls),
+
+    # Games App
+    path('', include('games.urls')),
+
+    #Accounts App
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    path('accounts/', include('accounts.urls')),
 ]
