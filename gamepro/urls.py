7931e9b62a1e54152c737de70caa6a75f95a8d9b
JericHunter <jeric.hunterr@gmail.com>
2019-12-11 22:14:58 -0800

    more static changes

--- gamepro/urls.py
+++ gamepro/urls.py
@@ -31,4 +31,7 @@ urlpatterns = [
 
     path('accounts/', include('accounts.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT), static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
