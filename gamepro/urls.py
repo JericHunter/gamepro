834f1ba9b5aafbfa64c44f4dab2f96b2e4bac58e
JericHunter <jeric.hunterr@gmail.com>
2019-12-11 22:11:06 -0800

    ughhH

--- gamepro/urls.py
+++ gamepro/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
 
     path('accounts/', include('accounts.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT), static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
